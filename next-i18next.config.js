3f64cd07d0e83129749348811d8b4233dbd3778b
SketchTurnerrr <nareknarek45@gmail.com>
2021-10-08 11:17:42 +0300

    fix i18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ua'],
+    localePath: path.resolve('./public/static/locales'),
   },
 };
